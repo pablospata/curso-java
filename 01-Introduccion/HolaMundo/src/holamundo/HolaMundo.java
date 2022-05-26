edab4787f3095a32b67acd729113d1900920ad49
Pablo Spata <pablospata@hotmail.com>
2022-05-26 18:58:20 -0300

    Actualizacion HolaMundo

--- 01-Introduccion/HolaMundo/src/holamundo/HolaMundo.java
+++ 01-Introduccion/HolaMundo/src/holamundo/HolaMundo.java
@@ -2,10 +2,12 @@
 package holamundo;
 
 public class HolaMundo {
-
+    
     public static void main(String[] args) {
+        
         System.out.println("Hola Mundo");
         System.out.println("Curso Java");//Con sout y Tab
-    }
+    
+    }   
     
 }
